53b6e03dc331a454cc9f68b4e97cd185f308a66f
XenonBlue <tullio.93@gmail.com>
2016-12-19 15:37:56 +0100

    Added different timers for animation and FPS

--- corePJ.cpp
+++ corePJ.cpp
@@ -18,7 +18,7 @@
 int main(int argc, char **argv)
 {
 	ALLEGRO_KEYBOARD_STATE	keyState;
-	ALLEGRO_MOUSE_STATE		mouseState;
+	//ALLEGRO_MOUSE_STATE		mouseState;
 
 	ALLEGRO_EVENT_QUEUE *eventQueue = NULL;
 
@@ -27,7 +27,7 @@ int main(int argc, char **argv)
 	if (!initAllegro())
 		return showMessageBox("Inizializzazione fallita a causa di un errore sconosciuto.", ALLEGRO_MESSAGEBOX_ERROR);
 
-	GameDisplay *gameDisplay = new GameDisplay(30.0);
+	GameDisplay *gameDisplay = new GameDisplay();
 
 	if (!gameDisplay->getDisplay())
 		return showMessageBox("Errore creazione display", ALLEGRO_MESSAGEBOX_ERROR);
@@ -42,12 +42,14 @@ int main(int argc, char **argv)
 	eventQueue = al_create_event_queue();
 
 	al_register_event_source(eventQueue, al_get_timer_event_source(gameDisplay->getTimerFPS()));
+	al_register_event_source(eventQueue, al_get_timer_event_source(gameDisplay->getTimerAnimation()));
 	al_register_event_source(eventQueue, al_get_display_event_source(gameDisplay->getDisplay()));
 	al_register_event_source(eventQueue, al_get_mouse_event_source());
 	al_register_event_source(eventQueue, al_get_keyboard_event_source());
 
 	gameDisplay->startSFX();
 
+	gameDisplay->startTimerAnimation();
 	gameDisplay->startTimerFPS();
 
 	while(!exitGame)
@@ -86,22 +88,31 @@ int main(int argc, char **argv)
 		}
 		else if (event.type == ALLEGRO_EVENT_TIMER)
 		{
-			gameDisplay->updateFPS();
+			if (event.timer.source == gameDisplay->getTimerFPS())
+			{
+				gameDisplay->updateFPS();
 
-			al_get_keyboard_state(&keyState);
-			al_get_mouse_state(&mouseState);
+				al_get_keyboard_state(&keyState);
+				//al_get_mouse_state(&mouseState);
 
-			if (al_key_down(&keyState, ALLEGRO_KEY_ESCAPE))
-				exitGame = true;
+				if (al_key_down(&keyState, ALLEGRO_KEY_ESCAPE))
+					exitGame = true;
 
-			p1->update();
+				p1->update();
 
-			f_bot->update(e_bot, p1);
+				f_bot->update(e_bot, p1);
 
-			if (e_bot->getDistance(p1) < e_bot->getDistance(f_bot))
-				e_bot->update(p1);
-			else
-				e_bot->update(f_bot);
+				if (e_bot->getDistance(p1) < e_bot->getDistance(f_bot))
+					e_bot->update(p1);
+				else
+					e_bot->update(f_bot);
+			}
+			else if (event.timer.source == gameDisplay->getTimerAnimation())
+			{
+				p1->updateAnimation();
+				f_bot->updateAnimation();
+				e_bot->updateAnimation();
+			}
 
 			render = true;
 		}
--- display.cpp
+++ display.cpp
@@ -7,12 +7,13 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_native_dialog.h>
 
-
 // TODO Fix icon
 
 class GameDisplay
 {
 	protected:
+	static const float		FPS = 60.;
+
 	ALLEGRO_DISPLAY 		*display;
 	ALLEGRO_DISPLAY_MODE	displayMode;
 	int						numDisplayModes;
@@ -21,25 +22,17 @@ class GameDisplay
 	float					gameTime;
 	int						frames,	gameFPS;
 
+	ALLEGRO_TIMER			*timerAnim;
+	float					animFPS;
+
 	ALLEGRO_FONT			*font[2];
-	ALLEGRO_SAMPLE			*sound;
+	ALLEGRO_SAMPLE			*sound[3];
+	ALLEGRO_SAMPLE_INSTANCE	*soundInstance[3];
 	//ALLEGRO_BITMAP			*icon;
 
 	public:
-	GameDisplay(const float FPS)
+	GameDisplay()
 	{
-		al_reserve_samples(1);
-		sound = al_load_sample("sfx/relax.wav");
-		//icon = al_load_bitmap("gfx/sheet/sun32.png");
-
-		font[0] = al_load_ttf_font("gfx/font/amsterdam.ttf", 36, 0);
-		font[1] = al_load_ttf_font("gfx/font/amsterdam.ttf", 15, 0);
-
-		timerFPS = al_create_timer(1.0f / FPS);
-		gameTime = 0.0f;
-		frames = 0;
-		gameFPS = 0;
-
 		al_inhibit_screensaver(true);
 		al_set_new_display_flags(ALLEGRO_FULLSCREEN_WINDOW);
 
@@ -49,6 +42,32 @@ class GameDisplay
 
 		//al_set_display_icon(display, icon);
 		al_set_window_title(display, "PiJam Engine (version A)");
+
+		al_reserve_samples(3);
+		sound[0] = al_load_sample("sfx/relax.wav");
+		sound[1] = al_load_sample("sfx/frogs.wav");
+		sound[2] = al_load_sample("sfx/sea.flac");
+
+		for (int i = 0; i < 3; i++)
+		{
+			soundInstance[i] = al_create_sample_instance(sound[i]);
+			al_set_sample_instance_playmode(soundInstance[i], ALLEGRO_PLAYMODE_LOOP);
+			al_attach_sample_instance_to_mixer(soundInstance[i], al_get_default_mixer());
+		}
+
+		//icon = al_load_bitmap("gfx/sheet/sun32.png");
+
+		font[0] = al_load_ttf_font("gfx/font/amsterdam.ttf", 36, 0);
+		font[1] = al_load_ttf_font("gfx/font/amsterdam.ttf", 15, 0);
+
+		animFPS = 10.;
+		timerAnim = al_create_timer(1.0 / animFPS);
+
+		timerFPS = al_create_timer(1.0f / FPS);
+		gameTime = 0.0f;
+		frames = 0;
+		gameFPS = 0;
+
 		al_hide_mouse_cursor(display);
 	}
 
@@ -57,9 +76,14 @@ class GameDisplay
 		al_show_mouse_cursor(display);
 		for (int i = 0; i < 2; i++)
 			al_destroy_font(font[i]);
-		//al_destroy_bitmap(icon);
-		al_destroy_sample(sound);
+		for (int i = 0; i < 3; i++)
+		{
+			al_destroy_sample_instance(soundInstance[i]);
+			al_destroy_sample(sound[i]);
+		}
 		al_destroy_timer(timerFPS);
+		al_destroy_timer(timerAnim);
+		//al_destroy_bitmap(icon);
 		al_destroy_display(display);
 		al_inhibit_screensaver(false);
 	}
@@ -85,14 +109,20 @@ class GameDisplay
 
 	void startSFX()
 	{
-		al_play_sample(sound, 1.0f, 0.0f, 1.0f,
-					ALLEGRO_PLAYMODE_LOOP, NULL);
+		al_play_sample_instance(soundInstance[0]);
+//		al_play_sample_instance(soundInstance[1]);
+//		al_play_sample(sound[0], 1.0f, 0.0f, 1.0f, ALLEGRO_PLAYMODE_LOOP, NULL);
 	}
 
 	void startTimerFPS()
 	{
 		gameTime = al_current_time();
 		al_start_timer(timerFPS);
+	}
+
+	void startTimerAnimation()
+	{
+		al_start_timer(timerAnim);
 	}
 
 	ALLEGRO_DISPLAY* getDisplay()
@@ -103,6 +133,11 @@ class GameDisplay
 	ALLEGRO_TIMER* getTimerFPS()
 	{
 		return timerFPS;
+	}
+
+	ALLEGRO_TIMER* getTimerAnimation()
+	{
+		return timerAnim;
 	}
 
 	ALLEGRO_FONT* getFont(int i)
