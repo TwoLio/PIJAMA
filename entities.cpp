6c210f0af8c0fcc06da08b8d8b1a22b827cc1f09
XenonBlue <tullio.93@gmail.com>
2016-12-22 00:14:39 +0100

    Added GameInput for Player

--- entities.cpp
+++ entities.cpp
@@ -1,4 +1,5 @@
 #include "object.cpp"
+#include "input.cpp"
 
 /*	TODO
 	~ Migliorare IA dei bot friendly;
@@ -15,10 +16,46 @@ class EnemyBot : public GameObject
 	public:
 	EnemyBot(float health, ALLEGRO_FONT *font,
 			obj_state state = IDLE, obj_type type = ENEMY,
+			obj_dir direction = WEST, int size = 32,
 			float sight = 100.0f, float speed = 1.f,
-			int size = 32,
 			float offsetX = 250.0f, float offsetY = 0.0f)	:
-	GameObject(health, font, state, type, sight, speed, size, offsetX, offsetY)	{}
+	GameObject(health, font, state, type, direction, size, sight, speed, offsetX, offsetY)	{}
+
+	void update(GameObject *target = NULL, GameObject *obj = NULL)
+	{
+		if (this->state == IDLE)
+		{
+			if (this->sight > this->getDistance(target))
+				this->state = CHASE;
+		}
+		else if (this->state == CHASE)
+		{
+			if (this->sight < this->getDistance(this->spawnX, this->spawnY))
+				this->state = WALK;
+			else
+			{
+				this->follow(target);
+
+				if (this->sight < this->getDistance(target))
+					this->state = WALK;
+			}
+		}
+		else if (this->state == WALK)
+		{
+			if (4 >= this->getDistance(this->spawnX, this->spawnY))
+			{
+				this->follow(this->spawnX, this->spawnY);
+				this->state = IDLE;
+			}
+			else
+			{
+				this->follow(this->spawnX, this->spawnY);
+
+				if (this->sight > this->getDistance(target))
+					this->state = CHASE;
+			}
+		}
+	}
 
 	void render(int r = 255, int g = 0, int b = 0)
 	{
@@ -35,43 +72,6 @@ class EnemyBot : public GameObject
 					ALLEGRO_ALIGN_RIGHT, "%i", (int)this->health);
 	}
 
-	void update(GameObject *target = NULL, GameObject *obj = NULL)
-	{
-		if (this->state == IDLE)
-		{
-			if (this->sight > this->getDistance(target))
-				this->state = CHASE;
-		}
-		else if (this->state == CHASE)
-		{
-			if (this->sight < this->getDistance(this->spawnX, this->spawnY))
-				this->state = RETREAT;
-			else
-			{
-				this->follow(target);
-
-				if (this->sight < this->getDistance(target))
-					this->state = RETREAT;
-			}
-		}
-		else if (this->state == RETREAT)
-		{
-			if (4 >= this->getDistance(this->spawnX, this->spawnY))
-			{
-				this->x = this->spawnX;
-				this->y = this->spawnY;
-				this->state = IDLE;
-			}
-			else
-			{
-				this->follow(this->spawnX, this->spawnY);
-
-				if (this->sight > this->getDistance(target))
-					this->state = CHASE;
-			}
-		}
-	}
-
 	void input()	{}
 	void move()		{}
 };
@@ -81,10 +81,45 @@ class Bot : public GameObject
 	public:
 	Bot(float health, ALLEGRO_FONT *font,
 		obj_state state = IDLE, obj_type type = FRIEND,
+		obj_dir direction = EAST, int size = 32,
 		float sight = 100.0f, float speed = 1.f,
-		int size = 32,
 		float offsetX = -250.0f, float offsetY = 20.0f)	:
-	GameObject(health, font, state, type, sight, speed, size, offsetX, offsetY)	{}
+	GameObject(health, font, state, type, direction, size, sight, speed, offsetX, offsetY)	{}
+
+	void update(GameObject *target = NULL, GameObject *obj = NULL)
+	{
+		if (this->state == IDLE)
+		{
+			if (this->sight > this->getDistance(obj))
+				this->state = WALK;
+		}
+		else if (this->state == ATTACK)
+		{
+			if (this->sight < this->getDistance(obj->x, obj->y))
+				this->state = WALK;
+			else
+			{
+				this->follow(target);
+
+				if (this->sight < this->getDistance(target))
+					this->state = WALK;
+			}
+		}
+		else if (this->state == WALK)
+		{	
+			if (64 >= this->getDistance(obj->x, obj->y))
+			{
+				this->state = IDLE;
+			}
+			else
+			{
+				this->follow(obj);
+
+				if (this->sight > this->getDistance(target))
+					this->state = ATTACK;
+			}
+		}
+	}
 
 	void render(int r = 0, int g = 255, int b = 0)
 	{
@@ -101,54 +136,47 @@ class Bot : public GameObject
 					ALLEGRO_ALIGN_RIGHT, "%i", (int)this->health);
 	}
 
-	void update(GameObject *target = NULL, GameObject *obj = NULL)
-	{
-		if (this->state == IDLE)
-		{
-			if (this->sight > this->getDistance(obj))
-				this->state = DEFEND;
-		}
-		else if (this->state == ATTACK)
-		{
-			if (this->sight < this->getDistance(obj->x, obj->y))
-				this->state = DEFEND;
-			else
-			{
-				this->follow(target);
-
-				if (this->sight < this->getDistance(target))
-					this->state = DEFEND;
-			}
-		}
-		else if (this->state == DEFEND)
-		{
-			if (64 >= this->getDistance(obj->x, obj->y))
-			{
-				this->state = IDLE;
-			}
-			else
-			{
-				this->follow(obj);
-
-				if (this->sight > this->getDistance(target))
-					this->state = ATTACK;
-			}
-		}
-	}
-
 	void input()	{}
 	void move()		{}
 };
 
 class Player : public GameObject
 {
+	protected:
+	GameInput	*gameInput;
+
 	public:
 	Player(float health, ALLEGRO_FONT *font,
 			obj_state state = WALK, obj_type type = PLAYER,
+			obj_dir direction = EAST, int size = 32,
 			float sight = 0.0f, float speed = 1.f,
-			int size = 32,
 			float offsetX = -250.0f, float offsetY = -20.0f)	:
-	GameObject(health, font, state, type, sight, speed, size, offsetX, offsetY)	{}
+	GameObject(health, font, state, type, direction, size, sight, speed, offsetX, offsetY)
+	{
+		this->gameInput = new GameInput();
+	}
+
+	~Player()
+	{
+		delete gameInput;
+	}
+
+	GameInput* getGameInput()
+	{
+		return this->gameInput;
+	}
+
+	void input(int keyCode, bool keyFlag)
+	{
+		this->getGameInput()->setKey(keyCode, keyFlag);
+	}
+
+	void update()
+	{
+		this->getGameInput()->getKeyState();
+		this->getGameInput()->getMouseState();
+		this->move();
+	}
 
 	void render(int r = 0, int g = 0, int b = 255)
 	{
@@ -161,246 +189,54 @@ class Player : public GameObject
 					ALLEGRO_ALIGN_RIGHT, "%i", (int)this->health);
 	}
 
-	void update()
-	{
-		this->move();
-	}
-
 	void move()
 	{
 		this->state = IDLE;
 
-		if (keys[UP])
+		if (this->getGameInput()->getKey(ALLEGRO_KEY_UP))
 		{
 			this->state = WALK;
 
-			if (keys[LEFT])
+			if (this->getGameInput()->getKey(ALLEGRO_KEY_LEFT))
 				this->follow(this->x - this->speed, this->y - this->speed);
-			else if (keys[RIGHT])
+			else if (this->getGameInput()->getKey(ALLEGRO_KEY_RIGHT))
 				this->follow(this->x + this->speed, this->y - this->speed);
 			else
 				this->follow(this->x, this->y - this->speed);
 		}
-		else if (keys[DOWN])
+		else if (this->getGameInput()->getKey(ALLEGRO_KEY_DOWN))
 		{
 			this->state = WALK;
 
-			if (keys[LEFT])
+			if (this->getGameInput()->getKey(ALLEGRO_KEY_LEFT))
 				this->follow(this->x - this->speed, this->y + this->speed);
-			else if (keys[RIGHT])
+			else if (this->getGameInput()->getKey(ALLEGRO_KEY_RIGHT))
 				this->follow(this->x + this->speed, this->y + this->speed);
 			else
 				this->follow(this->x, this->y + this->speed);
 		}
-		else if (keys[LEFT])
+		else if (this->getGameInput()->getKey(ALLEGRO_KEY_LEFT))
 		{
 			this->state = WALK;
 
-			if (keys[UP])
+			if (this->getGameInput()->getKey(ALLEGRO_KEY_UP))
 				this->follow(this->x - this->speed, this->y - this->speed);
-			else if (keys[DOWN])
+			else if (this->getGameInput()->getKey(ALLEGRO_KEY_DOWN))
 				this->follow(this->x - this->speed, this->y + this->speed);
 			else
 				this->follow(this->x - this->speed, this->y);
 		}
-		else if (keys[RIGHT])
+		else if (this->getGameInput()->getKey(ALLEGRO_KEY_RIGHT))
 		{
 			this->state = WALK;
 
-			if (keys[UP])
+			if (this->getGameInput()->getKey(ALLEGRO_KEY_UP))
 				this->follow(this->x + this->speed, this->y - this->speed);
-			else if (keys[DOWN])
+			else if (this->getGameInput()->getKey(ALLEGRO_KEY_DOWN))
 				this->follow(this->x + this->speed, this->y + this->speed);
 			else
 				this->follow(this->x + this->speed, this->y);
 		}
 
 	}
-
-	void input(int &keyCode, bool keyFlag)
-	{
-		switch (keyCode)
-		{
-		case ALLEGRO_KEY_UP:
-			keys[UP] = keyFlag;
-			break;
-		case ALLEGRO_KEY_DOWN:
-			keys[DOWN] = keyFlag;
-			break;
-		case ALLEGRO_KEY_LEFT:
-			keys[LEFT] = keyFlag;
-			break;
-		case ALLEGRO_KEY_RIGHT:
-			keys[RIGHT] = keyFlag;
-			break;
-		case ALLEGRO_KEY_SPACE:
-			keys[SPACE] = keyFlag;
-			break;
-		case ALLEGRO_KEY_ENTER:
-			keys[ENTER] = keyFlag;
-			break;
-		case ALLEGRO_KEY_ALT:
-			keys[ALT] = keyFlag;
-			break;
-		case ALLEGRO_KEY_ALTGR:
-			keys[ALTGR] = keyFlag;
-			break;
-		case ALLEGRO_KEY_ESCAPE:
-			keys[ESCAPE] = keyFlag;
-			break;
-		case ALLEGRO_KEY_BACKSPACE:
-			keys[BACKSPACE] = keyFlag;
-			break;
-		case ALLEGRO_KEY_LSHIFT:
-			keys[LSHIFT] = keyFlag;
-			break;
-		case ALLEGRO_KEY_RSHIFT:
-			keys[RSHIFT] = keyFlag;
-			break;
-		case ALLEGRO_KEY_LCTRL:
-			keys[LCTRL] = keyFlag;
-			break;
-		case ALLEGRO_KEY_RCTRL:
-			keys[RCTRL] = keyFlag;
-			break;
-		case ALLEGRO_KEY_TAB:
-			keys[TAB] = keyFlag;
-			break;
-		case ALLEGRO_KEY_DELETE:
-			keys[DEL] = keyFlag;
-			break;
-		case ALLEGRO_KEY_HOME:
-			keys[HOME] = keyFlag;
-			break;
-		case ALLEGRO_KEY_END:
-			keys[END] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PGUP:
-			keys[PGUP] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PGDN:
-			keys[PGDN] = keyFlag;
-			break;
-		case ALLEGRO_KEY_A:
-			keys[A] = keyFlag;
-			break;
-		case ALLEGRO_KEY_B:
-			keys[B] = keyFlag;
-			break;
-		case ALLEGRO_KEY_C:
-			keys[C] = keyFlag;
-			break;
-		case ALLEGRO_KEY_D:
-			keys[D] = keyFlag;
-			break;
-		case ALLEGRO_KEY_E:
-			keys[E] = keyFlag;
-			break;
-		case ALLEGRO_KEY_F:
-			keys[F] = keyFlag;
-			break;
-		case ALLEGRO_KEY_G:
-			keys[G] = keyFlag;
-			break;
-		case ALLEGRO_KEY_H:
-			keys[H] = keyFlag;
-			break;
-		case ALLEGRO_KEY_I:
-			keys[I] = keyFlag;
-			break;
-		case ALLEGRO_KEY_J:
-			keys[J] = keyFlag;
-			break;
-		case ALLEGRO_KEY_K:
-			keys[K] = keyFlag;
-			break;
-		case ALLEGRO_KEY_L:
-			keys[L] = keyFlag;
-			break;
-		case ALLEGRO_KEY_M:
-			keys[M] = keyFlag;
-			break;
-		case ALLEGRO_KEY_N:
-			keys[N] = keyFlag;
-			break;
-		case ALLEGRO_KEY_O:
-			keys[O] = keyFlag;
-			break;
-		case ALLEGRO_KEY_P:
-			keys[P] = keyFlag;
-			break;
-		case ALLEGRO_KEY_Q:
-			keys[Q] = keyFlag;
-			break;
-		case ALLEGRO_KEY_R:
-			keys[R] = keyFlag;
-			break;
-		case ALLEGRO_KEY_S:
-			keys[S] = keyFlag;
-			break;
-		case ALLEGRO_KEY_T:
-			keys[T] = keyFlag;
-			break;
-		case ALLEGRO_KEY_U:
-			keys[U] = keyFlag;
-			break;
-		case ALLEGRO_KEY_V:
-			keys[V] = keyFlag;
-			break;
-		case ALLEGRO_KEY_W:
-			keys[W] = keyFlag;
-			break;
-		case ALLEGRO_KEY_X:
-			keys[X] = keyFlag;
-			break;
-		case ALLEGRO_KEY_Y:
-			keys[Y] = keyFlag;
-			break;
-		case ALLEGRO_KEY_Z:
-			keys[Z] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_0:
-			keys[PAD_0] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_1:
-			keys[PAD_1] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_2:
-			keys[PAD_2] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_3:
-			keys[PAD_3] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_4:
-			keys[PAD_4] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_5:
-			keys[PAD_5] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_6:
-			keys[PAD_6] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_7:
-			keys[PAD_7] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_8:
-			keys[PAD_8] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_9:
-			keys[PAD_9] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_ENTER:
-			keys[PAD_ENTER] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_DELETE:
-			keys[PAD_DEL] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_PLUS:
-			keys[PAD_PLUS] = keyFlag;
-			break;
-		case ALLEGRO_KEY_PAD_MINUS:
-			keys[PAD_MINUS] = keyFlag;
-			break;
-		}
-	}	
 };
