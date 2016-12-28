eff9fb16b504d36643953f42a2e6e292ac0149f5
XenonBlue <tullio.93@gmail.com>
2016-12-28 20:28:03 +0100

    Load state for map loading function

--- globals.h
+++ globals.h
@@ -1,5 +1,19 @@
 #pragma once
+
 #include <allegro5/allegro.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_native_dialog.h>
+#include <allegro5/allegro_opengl.h>
+
+//#include <glm/vec3.hpp> // glm::vec3
+//#include <glm/vec4.hpp> // glm::vec4
+//#include <glm/mat4x4.hpp> // glm::mat4
+//#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
 
 #define DISPLAY	al_get_current_display()
 
@@ -7,12 +21,19 @@ const float PI = ALLEGRO_PI;
 const float H_PI = ALLEGRO_PI/2.;
 const float OS_PI = ALLEGRO_PI/8.;		//	Offset
 
-enum game_states
+enum game_state
 {
 	MENU,
 	PLAY,
 	PAUSE,
 	CREDIT
+};
+
+enum load_state
+{
+	NAME,
+	SIZE,
+	MAP
 };
 
 enum obj_type
