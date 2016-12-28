bc216c59470243c91409a1d4d1003101fe338489
XenonBlue <tullio.93@gmail.com>
2016-12-28 20:40:21 +0100

    Update

--- corePJ.cpp
+++ corePJ.cpp
@@ -1,13 +1,3 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <allegro5/allegro_opengl.h>
-
 #include "game.cpp"
 
 int main(int argc, char **argv)
