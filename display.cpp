5c3ac1078e56de703cc6b94a84021285129da752
XenonBlue <tullio.93@gmail.com>
2016-12-28 20:33:51 +0100

    Update

--- display.cpp
+++ display.cpp
@@ -1,12 +1,3 @@
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_native_dialog.h>
-
 #include "entities.cpp"
 
 // TODO Fix icon
