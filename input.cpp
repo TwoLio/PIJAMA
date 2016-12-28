fef2ebe997928b7135d2d6ae1e60c887f520b11d
XenonBlue <tullio.93@gmail.com>
2016-12-28 20:30:43 +0100

    Update

--- input.cpp
+++ input.cpp
@@ -1,13 +1,4 @@
-#include <iostream>
-
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_ttf.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_audio.h>
-#include <allegro5/allegro_acodec.h>
-#include <allegro5/allegro_primitives.h>
-#include <allegro5/allegro_native_dialog.h>
+#include "globals.h"
 
 class GameInput
 {
